849542470806fab54279a6449f12617edde05d40
“MelkerAndreasson” <ma22tq@student.lnu.se>
2023-05-01 00:04:19 +0200

    Add class for saving documents to Pinecone

--- src/app.js
+++ src/app.js
@@ -1,29 +1,64 @@
+/* eslint-disable jsdoc/require-jsdoc */
 import { OpenAI } from 'langchain/llms/openai'
 import dotenv from 'dotenv'
 import { createRequire } from 'module'
-import { PdfLoader } from '../dataloaders/pdfloader.js'
+import { PdfLoader } from './dataloaders/pdfloader.js'
+import { TextSplitter } from './textsplitter/textsplitter.js'
+import { VectorConverter } from './vectorconverter/vectorconverter.js'
 import path from 'path'
 
 createRequire(import.meta.url)
 
-dotenv.config()
+async function loadEnvironmentVariables() {
+  dotenv.config({ path: '../.env' })
+}
 
-// try {
-//   const model = new OpenAI({
-//     openAIApiKey: `${process.env.OPENAI_API_KEY}`,
-//     temperature: 0.9,
-//   })
+try {
+  await loadEnvironmentVariables()
 
-//   const res = await model.call(
-//     'What would be a good company name a company that makes colorful socks?'
-//   )
-//   console.log(res)
-// } catch (error) {
-//   console.log(error)
-// }
+  // dotenv.config()
 
-const pdfLoader = new PdfLoader()
-const filePath = 'src/utbildningsplan-NGWEC-7.pdf'
-const absolutePath = path.resolve(filePath)
+  // try {
+  //   const model = new OpenAI({
+  //     openAIApiKey: `${process.env.OPENAI_API_KEY}`,
+  //     temperature: 0.9,
+  //   })
 
-console.log(await pdfLoader.load(`${absolutePath}`))
+  //   const res = await model.call(
+  //     'What would be a good company name a company that makes colorful socks?'
+  //   )
+  //   console.log(res)
+  // } catch (error) {
+  //   console.log(error)
+  // }
+
+  const pdfLoader = new PdfLoader()
+  const filePath = 'src/utbildningsplan-NGWEC-7.pdf'
+  const absolutePath = path.resolve(filePath)
+
+  console.log(await pdfLoader.load(`${absolutePath}`))
+
+  const textSplitter = new TextSplitter()
+
+  const text = `Hi.\n\nI'm Harrison.\n\nHow? Are? You?\nOkay then f f f f.
+  This is a weird text to write, but gotta test the splittingggg some how.\n\n
+  Bye!\n\n-H.`
+
+  console.log(await textSplitter.splitText(text, 20))
+
+  const documents = []
+
+  const doc = await textSplitter.splitText(text, 20)
+
+  documents.push(doc)
+
+  const vectorConverter = new VectorConverter()
+
+  for (const document of documents) {
+    if (document !== null) {
+      await vectorConverter.convert(document)
+    }
+  }
+} catch (error) {
+  console.log(error)
+}
---
+++ src/textsplitter/textsplitter.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsdoc/require-jsdoc */
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { Document } from 'langchain/document'
+
+export class TextSplitter {
+//   textSplitter
+
+//   constructor() {
+//     this.textSplitter = new RecursiveCharacterTextSplitter({
+//       chunkSize: 1000,
+//       chunkOverlap: 200,
+//     })
+//   }
+
+  //   async split(text) {
+  //     // return await this.textSplitter.splitDocuments([
+  //     //   new Document({ pageContent: text })
+  //     // ])
+  //     return await this.textSplitter.createDocuments([text])
+  //   }
+
+  async splitText(text, chunkSize) {
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize,
+      chunkOverlap: 1
+    })
+
+    return await splitter.createDocuments([text])
+  }
+}
