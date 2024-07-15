3c10459a60c35b09867278d70b44cdb60e4369e4
ma225tq <ma225tq@student.lnu.se>
2024-07-15 17:30:10 +0200

    update

--- src/functions/database-interaction/vectorManager.js
+++ src/functions/database-interaction/vectorManager.js
@@ -1,13 +1,21 @@
 /* eslint-disable jsdoc/require-jsdoc */
-import { PineconeClient } from '@pinecone-database/pinecone'
+import { Pinecone } from '@pinecone-database/pinecone'
+import { OpenAIEmbeddings, OpenAI } from '@langchain/openai'
+import { PineconeStore } from '@langchain/pinecone'
 import * as dotenv from 'dotenv'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { PineconeStore } from 'langchain/vectorstores/pinecone'
 import { VectorDBQAChain } from 'langchain/chains'
-import { OpenAI } from 'langchain/llms/openai'
-import CryptoJS from 'crypto-js'
-import { DocumentIdentifier } from '../../../models/document-id.js'
 import { getIo } from '../../socket.js'
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate
+} from '@langchain/core/prompts'
+import {
+  RunnablePassthrough,
+  RunnableSequence
+} from '@langchain/core/runnables'
+import { formatDocumentsAsString } from 'langchain/util/document'
+import { StringOutputParser } from '@langchain/core/output_parsers'
 
 dotenv.config()
 
@@ -17,52 +25,90 @@ export class VectorManager {
   #initialized
   socket
 
-  constructor() {
+  constructor () {
     this.#initialized = this.#init()
   }
 
-  async #init() {
-    this.#client = new PineconeClient()
-    await this.#client.init({
-      apiKey: process.env.PINECONE_API_KEY,
-      environment: process.env.PINECONE_ENVIRONMENT,
+  async #init () {
+    this.#client = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY
     })
+
     this.#pineconeIndex = this.#client.Index(process.env.PINECONE_INDEX)
   }
 
-  async index(docs, uid) {
+  async index (docs, uid) {
     await this.#initialized
 
-    // for (const doc of docs) {
-    //   doc.pageContent.replace((/\n/g, ' '))
-    //   console.log(doc.metadata)
-    // }
-
-    await PineconeStore.fromDocuments(docs, new OpenAIEmbeddings(), {
-      pineconeIndex: this.#pineconeIndex,
-      namespace: `${uid}`,
-    })
+    try {
+      await PineconeStore.fromDocuments(docs, new OpenAIEmbeddings(), {
+        pineconeIndex: this.#pineconeIndex,
+        namespace: `${uid}`
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  async query(query) {
+  async query (query) {
     await this.#initialized
     const vectorStore = await PineconeStore.fromExistingIndex(
       new OpenAIEmbeddings(),
       { pineconeIndex: this.#pineconeIndex }
     )
 
-    const model = new OpenAI()
+    const vectorStoreRetriever = vectorStore.asRetriever()
 
-    const chain = VectorDBQAChain.fromLLM(model, vectorStore, {
-      k: 1,
-      returnSourceDocuments: true,
+    const SYSTEM_TEMPLATE = `Use the following pieces of context to answer the question at the end.
+                              If you don't know the answer, just say that you don't know, don't try to make up an answer.
+                              ----------------
+                              {context}`
+
+    const messages = [
+      SystemMessagePromptTemplate.fromTemplate(SYSTEM_TEMPLATE),
+      HumanMessagePromptTemplate.fromTemplate('{question}')
+    ]
+    const prompt = ChatPromptTemplate.fromMessages(messages)
+
+    const model = new OpenAI({
+      maxTokens: -1,
+      streaming: true
     })
-    const response = await chain.call({ query: `${query}` })
-    return response
+
+    const chain = RunnableSequence.from([
+      {
+        context: vectorStoreRetriever.pipe(formatDocumentsAsString),
+        question: new RunnablePassthrough()
+      },
+      prompt,
+      model,
+      new StringOutputParser()
+    ])
+
+    const answer = await chain.invoke(
+      'say hello if you are receiving this'
+    )
+
+    console.log(answer)
+
+    return answer
+
+    // const results = await vectorStore.similaritySearch(`${query}`, 1, {
+    // });
+    // console.log(results);
+
+    // const model = new OpenAI()
+
+    // const chain = VectorDBQAChain.fromLLM(model, vectorStore, {
+    //   k: 1,
+    //   returnSourceDocuments: true
+    // })
+    // const response = await chain.call({ query: `${query}` })
+    // return response
     // console.log(response)
   }
 
-  async queryWithStreaming(query, uid) {
+  async queryWithStreaming (query, uid) {
     this.socket = getIo()
     await this.#initialized
     const vectorStore = await PineconeStore.fromExistingIndex(
@@ -72,12 +118,12 @@ export class VectorManager {
 
     const model = new OpenAI({
       maxTokens: -1,
-      streaming: true,
+      streaming: true
     })
 
     const chain = VectorDBQAChain.fromLLM(model, vectorStore, {
       k: 1,
-      returnSourceDocuments: true,
+      returnSourceDocuments: true
     })
 
     this.socket.emit('responseStart')
@@ -85,10 +131,10 @@ export class VectorManager {
     const response = await chain.call({ query: `${query}` }, [
       {
         handleLLMNewToken: (token) => {
-          this.socket.emit('newToken', { token: token, type: 'server' })
-          console.log({ token: token, type: 'server' })
-        },
-      },
+          this.socket.emit('newToken', { token, type: 'server' })
+          console.log({ token, type: 'server' })
+        }
+      }
     ])
     // this.socket.emit('responseComplete')
     return response
@@ -96,12 +142,12 @@ export class VectorManager {
     // console.log(response)
   }
 
-  async deleteNamespace(uid) {
+  async deleteNamespace (uid) {
     await this.#initialized
 
     await this.#pineconeIndex.delete1({
       deleteAll: true,
-      namespace: `${uid}`,
+      namespace: `${uid}`
     })
   }
 }
